d0b79c7bcb7a930782b65f84e5badc82c9c5845a
matraass11 <fedor.kurmanov@gmail.com>
2026-02-17 19:24:10 +0100

    changes to coordinates - the boxes now cover a bit more

--- coordinates.js
+++ coordinates.js
@@ -1,13 +1,13 @@
 // --- OVERLAY LAYOUT DATA ---
 const OVERLAY_COORDS = {
     1: { // Mode 1: Standard Spoilers
-        "top-horizontal-strip": { xStart: 5, xEnd: 38, yStart: 5.5, yEnd: 10 },
-        "left-vertical-strip": { xStart: 5, xEnd: 15, yStart: 12, yEnd: 31 },
+        "top-horizontal-strip": { xStart: 1, xEnd: 50, yStart: 3, yEnd: 10 },
+        "left-vertical-strip": { xStart: 1, xEnd: 15, yStart: 11, yEnd: 31 },
         "bottom-wide-strip": { xStart: 10, xEnd: 95, yStart: 79, yEnd: 99 }
     },
     2: { // Mode 2: Night Mode
-        "night-left-block": { xStart: 5, xEnd: 38, yStart: 5.5, yEnd: 10 },
-        "night-bottom-block": { xStart: 5, xEnd: 95, yStart: 12, yEnd: 99 }
+        "night-left-block": { xStart: 1, xEnd: 50, yStart: 3, yEnd: 10 },
+        "night-bottom-block": { xStart: 1, xEnd: 95, yStart: 11, yEnd: 99 }
     },
     3: {} // Mode 3: Clear (No coordinates)
 };
