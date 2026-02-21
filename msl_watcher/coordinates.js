7a94ca0ce3cc2341bb72df8955b418c11dcec152
matraass11 <fedor.kurmanov@gmail.com>
2026-02-21 16:29:17 +0100

    fixed the box coordinates

--- msl_watcher/coordinates.js
+++ msl_watcher/coordinates.js
@@ -1,9 +1,9 @@
 // --- OVERLAY LAYOUT DATA ---
 const OVERLAY_COORDS = {
     1: { // Mode 1: Standard Spoilers
-        "top-horizontal-strip": { xStart: 1, xEnd: 50, yStart: 3, yEnd: 10 },
-        "left-vertical-strip": { xStart: 1, xEnd: 15, yStart: 11, yEnd: 31 },
-        "bottom-wide-strip": { xStart: 10, xEnd: 95, yStart: 79, yEnd: 99 }
+        "top-horizontal-strip": { xStart: 0, xEnd: 50, yStart: 0, yEnd: 11 },
+        "left-vertical-strip": { xStart: 0, xEnd: 15, yStart: 11, yEnd: 31 },
+        "bottom-wide-strip": { xStart: 10, xEnd: 95, yStart: 79, yEnd: 100 }
     },
     2: { // Mode 2: Night Mode
         "night-left-block": { xStart: 1, xEnd: 50, yStart: 3, yEnd: 10 },
